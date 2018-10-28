564ac585de61807260cc1d08fcae1b4efe56ae90
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-10-29 00:13:54 +0200

    Create MainProgramm.java

---
+++ MainProgramm.java
@@ -0,0 +1,23 @@
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class MainProgramm {
+	public static void main (String[] arguments) throws
+	NoSuchAlgorithmException,NoSuchPaddingException, InvalidKeyException,
+	UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
+		Abonent abonent1 = new Abonent();
+		Abonent abonent2 = new Abonent();
+		
+		abonent1.exchangeKeys(abonent2);
+		byte[] x = abonent1.encrypt("Hello abonent2!\n");
+		System.out.print(x + "\n");
+		
+		String y = abonent2.decrypt(x);
+		System.out.print(y + "\n");		
+	}
+}
