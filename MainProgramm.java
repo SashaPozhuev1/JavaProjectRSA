0925a3151aa39f337cecc223f55939d7ed39c8d2
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-11-02 21:12:51 +0200

    Update MainProgramm.java

--- MainProgramm.java
+++ MainProgramm.java
@@ -13,14 +13,14 @@ public class MainProgramm {
 		Abonent abonent1 = new Abonent();
 		Abonent abonent2 = new Abonent();
 		
-		// каждый раз разные ключи
 		for(int i = 0; i < 2; i++) {
 			abonent1.exchangeKeys(abonent2);
+			
 			byte[] x = abonent1.encrypt("Hello abonent2!\n");
 			System.out.print(x + "\n");
 		
 			String y = abonent2.decrypt(x);
 			System.out.print(y + "\n");		
-		}		
+		}
 	}
 }
