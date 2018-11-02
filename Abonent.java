337b129b38af2823d327098e65012b9f1a557d8a
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-11-02 21:12:15 +0200

    Update Abonent.java

--- Abonent.java
+++ Abonent.java
@@ -10,55 +10,47 @@ public class Abonent {
 	public PublicKey openKey_;
 	private PrivateKey secretKey_;
 	
-	//public PublicKey otherOpenKey_;// ?
+	public PublicKey otherOpenKey_;
 
 	private byte[] openMessage_;	  // пишем свои, расшифровываем чужие
 	private byte[] secretMessage_; // приходят чужие, отправляем свои
 	
+	private SecureRandom secureRandom;
+	
 	// необходимо различать свои и чужие сообщения
 	public Abonent() throws NoSuchAlgorithmException, NoSuchPaddingException{
 		cipher_ = Cipher.getInstance( "RSA" );
-	}
-	
-	public void exchangeKeys(Abonent other) throws NoSuchAlgorithmException{
-		makeKeyGen(other);
-	}
-	
-	private void makeKeyGen(Abonent other) throws NoSuchAlgorithmException{
+		
 		keyGen_ = KeyPairGenerator.getInstance( "RSA" );
-		keyGen_.initialize(2048);
+		keyGen_.initialize(2048, secureRandom);
 		
 		pg_ = keyGen_.genKeyPair();		
 		openKey_ = pg_.getPublic();
-	    	secretKey_ = pg_.getPrivate();
-	    
-	    	other.makeMyKey(pg_, openKey_);
+	    secretKey_ = pg_.getPrivate();
+	 //   System.out.print("opk:" + openKey_ + "\n" + "sck"+ secretKey_ + "\n");
 	}
 	
-	// тут лучше попросить вызвать другой приватный метод и отобрать открытые ключи у собеседника
-	public void makeMyKey(KeyPair pg, PublicKey openKey) {
-		pg_ = pg;
-		openKey_ = openKey;
-		setSecretKey();
-		//other.otherOpenKey_ = openKey_; // нет?
+	public void exchangeKeys(Abonent other) throws NoSuchAlgorithmException{
+		otherOpenKey_ = other.makeMyKey(openKey_);	
 	}
 	
-	private void setSecretKey() {
-		secretKey_ = pg_.getPrivate();
+	public PublicKey makeMyKey(PublicKey openKey) {
+		otherOpenKey_ = openKey;
+		return openKey_;
 	}
 	
 	public byte[] encrypt(String openMessage) throws InvalidKeyException,
 	UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException{
-		cipher_.init( Cipher.ENCRYPT_MODE, openKey_ );
+		cipher_.init( Cipher.ENCRYPT_MODE, otherOpenKey_ );
 		openMessage_ = openMessage.getBytes("UTF-8");    // а надо ли?
-	    	secretMessage_ = cipher_.doFinal( openMessage_ );//
-	    	return secretMessage_; // если что, можно toString()
+	    secretMessage_ = cipher_.doFinal( openMessage_ );//
+	    return secretMessage_; // если что, можно toString()
 	}
 	
 	public String decrypt(byte[] secretMessage) throws InvalidKeyException,
 	IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException{
 		cipher_.init( Cipher.DECRYPT_MODE, secretKey_ );	
-	    	openMessage_ = cipher_.doFinal(secretMessage);
-	    	return new String(openMessage_, "UTF-8");
+	    openMessage_ = cipher_.doFinal(secretMessage);
+	    return new String(openMessage_, "UTF-8");
 	}
 }
