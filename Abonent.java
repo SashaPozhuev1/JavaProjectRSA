82f317b8650fd9d44a2f7668682003343fe62fc8
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-11-17 03:06:15 +0200

    Update Abonent.java

--- Abonent.java
+++ Abonent.java
@@ -1,106 +1,115 @@
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-
-import java.io.UnsupportedEncodingException;
 import java.security.*;
 
+import org.apache.commons.codec.binary.Base64;
+
 public class Abonent {
-	private SecureRandom secureRandom;
-	static private int counter_ = 0;
-	//private Boolean firstAbonent_ = false;
-	static private Abonent mainAbonent_;
-	
-	// RSA
-	private Cipher cipher_;
-	private KeyPairGenerator keyGen_;
-	
-	private KeyPair pg_;
-	private PublicKey openKey_;
-	private PrivateKey secretKey_;
-	// private PublicKey otherOpenKey_;
-	
-	// AES
-	String sessionKey_ = "Bar12345Bar12345"; // 128 bit key
-    	String sessionInitVector_ = "RandomInitVector"; // 16 bytes
-	
-	public Abonent() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException{
-		// RSA key generation
-		secureRandom = new SecureRandom(); // insert byte;
-		cipher_ = Cipher.getInstance( "RSA" );
-		
-		keyGen_ = KeyPairGenerator.getInstance( "RSA" );
-		keyGen_.initialize(2048, secureRandom);
-		
-		pg_ = keyGen_.genKeyPair();		
-		openKey_ = pg_.getPublic();
-	    secretKey_ = pg_.getPrivate();
-	    
-	    // AES key generation
-	    if(counter_ == 0) {	
-	    	mainAbonent_ = this;
-	    	// firstAbonent_ = true;
-	    	// randomize sessionKey_;
-	    	// randomize sessionInitVector_;
-	    }
-	    else{
-	    	getSession(mainAbonent_, openKey_);
-	    }
-	    counter_++;
-	    System.out.print("opk:" + openKey_ + "\n" + "sck"+ secretKey_ + "\n");
-	}
-	
-	private void getSession(Abonent mainAbonent, PublicKey openKey) throws InvalidKeyException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
-		// decrypt
-		sessionKey_ = new String(decryptRSA(mainAbonent.encryptRSA(mainAbonent.sessionKey_, openKey)));
-		sessionInitVector_ = new String(decryptRSA(mainAbonent.encryptRSA(mainAbonent.sessionInitVector_, openKey)));
-	}
+    private SecureRandom secureRandom_;
+    static private int counter_ = 0;
+    static private Abonent mainAbonent_;
+    
+    // AES
+    private String[] sessionPair_ = new String[2];
+    
+    public Abonent() {
+        try {
+            // AES key generation
+            if(counter_ == 0) {    
+                mainAbonent_ = this;
+                sessionPair_[0] = "Bar12345Bar12345"; //randomStringGenerator.generate(16); // 128 bit key; // randomize
+                sessionPair_[1] = "RandomInitVector"; //randomStringGenerator.generate(16); // 16 bytes; // randomize
+            }
+            else{
+                // RSA key generation
+                secureRandom_ = new SecureRandom(); // mb insert byte;
+                KeyPairGenerator keyGen = KeyPairGenerator.getInstance( "RSA" ); 
+                keyGen.initialize(2048, secureRandom_);
+                KeyPair pg = keyGen.genKeyPair();
+                Cipher cipher = Cipher.getInstance( "RSA" );
+                   
+                getSession(mainAbonent_, pg, cipher);
+            }
+            counter_++;
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    private void getSession(Abonent mainAbonent,KeyPair pg, Cipher cipher) {
+        try {
+            for(int i = 0; i < 2; i++) {
+                sessionPair_[i] = new String(
+                    decryptRSA(
+                            mainAbonent.encryptRSA(i, pg.getPublic(), cipher),
+                            pg.getPrivate(),
+                            cipher
+                            )
+                    );
+            }
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	public byte[] encryptRSA(String openMessage, PublicKey openKey) throws InvalidKeyException,
-	UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException{
-		cipher_.init( Cipher.ENCRYPT_MODE, openKey );
-		return cipher_.doFinal( openMessage.getBytes("UTF-8") ); // or toString()
-	}
+    // RSA methods
+    private byte[] encryptRSA(int elem, PublicKey openKey, Cipher cipher){
+        try {
+            cipher.init( Cipher.ENCRYPT_MODE, openKey );
+            return cipher.doFinal( sessionPair_[elem].getBytes("UTF-8") ); // or toString()
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
-	public String decryptRSA(byte[] secretMessage) throws InvalidKeyException,
-	IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException{
-		cipher_.init( Cipher.DECRYPT_MODE, secretKey_ );
-		return new String(cipher_.doFinal(secretMessage), "UTF-8");
-	}
+    private String decryptRSA(byte[] secretMessage, PrivateKey secretKey, Cipher cipher){
+        try {
+        cipher.init( Cipher.DECRYPT_MODE, secretKey );
+        return new String(cipher.doFinal(secretMessage), "UTF-8");
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
-	public static String encrypt(String key, String initVector, String value) {
-		try {
-			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-			SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+    // AES methods
+    public String encrypt(String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(sessionPair_[1].getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(sessionPair_[0].getBytes("UTF-8"), "AES");
 
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-			cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
 
-			byte[] encrypted = cipher.doFinal(value.getBytes("UTF-8"));
+            byte[] encrypted = cipher.doFinal(value.getBytes());
 
-			return new String(encrypted, "UTF-8");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+            return Base64.encodeBase64String(encrypted);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 
-		return null;
-	}
+    public String decrypt(String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(sessionPair_[1].getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(sessionPair_[0].getBytes("UTF-8"), "AES");
 
-	public static String decrypt(String key, String initVector, String encrypted) {
-		try {
-			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
-			SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
-			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-			cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
 
-			byte[] original = cipher.doFinal(encrypted.getBytes("UTF-8"));
-
-			return new String(original, "UTF-8");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-
-		return null;
-	}
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
