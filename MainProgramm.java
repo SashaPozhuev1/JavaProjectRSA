69ad4ff50b68e142888d92feb791042aa4174ffb
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-11-17 03:07:20 +0200

    Update MainProgramm.java

--- MainProgramm.java
+++ MainProgramm.java
@@ -1,26 +1,19 @@
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-
 public class MainProgramm {
-	public static void main (String[] arguments) throws
-	NoSuchAlgorithmException,NoSuchPaddingException, InvalidKeyException,
-	UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
-		Abonent abonent1 = new Abonent();
-		Abonent abonent2 = new Abonent();
-		
-		for(int i = 0; i < 2; i++) {
-			abonent1.exchangeKeys(abonent2);
-			
-			byte[] x = abonent1.encrypt("Hello abonent2!\n");
-			System.out.print(x + "\n");
-		
-			String y = abonent2.decrypt(x);
-			System.out.print(y + "\n");		
-		}
-	}
+    public static void main (String[] arguments){
+        try {
+            Abonent abonent1 = new Abonent();
+            Abonent abonent2 = new Abonent();
+        
+            for(int i = 0; i < 2; i++) {
+                String x = abonent1.encrypt("Hello abonent2!" + i);
+                System.out.print(x + "\n");
+        
+                String y = abonent2.decrypt(x);
+                System.out.print(y + "\n");        
+            }
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
