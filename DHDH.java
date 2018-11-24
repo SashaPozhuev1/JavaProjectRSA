aa190fc03dadab2372bef09287f14dfb403d59d5
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2018-11-25 01:43:49 +0200

    Update DHDH.java

--- DHDH.java
+++ DHDH.java
@@ -1,14 +1,16 @@
  public void DHDH() throws Exception {
     // АЛИСА
-    	// АЛИСА создаёт ключ 2048 бит, не забыть про secureRandom
+    	// АЛИСА создаёт ключ 2048 бит
+    	SecureRandom secureRandom = new SecureRandom();
         KeyPairGenerator aliceKpairGen = KeyPairGenerator.getInstance("DH");
-        aliceKpairGen.initialize(2048);
+        aliceKpairGen.initialize(2048, secureRandom);
         KeyPair aliceKpair = aliceKpairGen.generateKeyPair();
         
         // АЛИСА создаёт DH KeyAgreement объект (приватный ключ) и инвертирует публичный ключ в байты
         KeyAgreement aliceKeyAgree = KeyAgreement.getInstance("DH");
         aliceKeyAgree.init(aliceKpair.getPrivate());
-        byte[] alicePubKeyEnc = aliceKpair.getPublic().getEncoded(); // отправляем это Бобу
+        // отправляем это Бобу
+        byte[] alicePubKeyEnc = aliceKpair.getPublic().getEncoded(); 
     // БОБ    
         // Боб из байтов АЛИСЫ формирует её публичный ключ 
         KeyFactory bobKeyFac = KeyFactory.getInstance("DH");
@@ -24,7 +26,7 @@
         // БОБ создаёт DH KeyAgreement объект (приватный ключ) и инвертирует публичный ключ в байты
         KeyAgreement bobKeyAgree = KeyAgreement.getInstance("DH");
         bobKeyAgree.init(bobKpair.getPrivate());
-        byte[] bobPubKeyEnc = bobKpair.getPublic().getEncoded();
+        byte[] bobPubKeyEnc = bobKpair.getPublic().getEncoded(); //+
 
     // АЛИСА
         // АЛИСА из байтов БОБА формирует его публичный ключ и подключает к общему секрету
@@ -39,67 +41,40 @@
 
     // ОБА
         // формируют общий секрет, он получается одинаковым
-        	byte[] aliceSharedSecret = aliceKeyAgree.generateSecret();
-        	byte[] bobSharedSecret = bobKeyAgree.generateSecret();
+       	byte[] aliceSharedSecret = aliceKeyAgree.generateSecret();
+       	byte[] bobSharedSecret = bobKeyAgree.generateSecret();
             
-            System.out.println("Alice secret: " +
-                toHexString(aliceSharedSecret));
-            System.out.println("Bob secret: " +
-                toHexString(bobSharedSecret));
-            if (!java.util.Arrays.equals(aliceSharedSecret, bobSharedSecret))
-            	throw new Exception("Shared secrets differ");
-            System.out.println("Shared secrets are the same");
+        if (!java.util.Arrays.equals(aliceSharedSecret, bobSharedSecret))
+           	throw new Exception("Shared secrets differ");
+        System.out.println("Shared secrets are the same");
         
         // теперь на основе общего секрета они могут создать AES ключ
-        SecretKeySpec bobAesKey = new SecretKeySpec(bobSharedSecret, 0, 16, "AES");			//получение AES ключа на основе общего секрета
+        SecretKeySpec bobAesKey = new SecretKeySpec(bobSharedSecret, 0, 16, "AES");			
         SecretKeySpec aliceAesKey = new SecretKeySpec(aliceSharedSecret, 0, 16, "AES");
 
-        // и передавать сообщения
+    // БОБ
+        // создаёт шифр, применяет его и параметры шифрования
         Cipher bobCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         bobCipher.init(Cipher.ENCRYPT_MODE, bobAesKey);
-        
-        byte[] cleartext = "This is just an example".getBytes();
-        byte[] ciphertext = bobCipher.doFinal(cleartext);
-
-        // Retrieve the parameter that was used, and transfer it to Alice in
-        // encoded format
+        // нужно передать зашифрованный текст и параметры шифрования
+        byte[] ciphertext = bobCipher.doFinal("ПРИМЕР СООБЩЕНИЯ".getBytes());
         byte[] encodedParams = bobCipher.getParameters().getEncoded();
 
-        /*
-         * Alice decrypts, using AES in CBC mode
-         */
-
-        // Instantiate AlgorithmParameters object from parameter encoding
-        // obtained from Bob
+    // АЛИСА 
+        // применяет параметры шифрования и свой AES ключ
         AlgorithmParameters aesParams = AlgorithmParameters.getInstance("AES");
         aesParams.init(encodedParams);
+        
         Cipher aliceCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         aliceCipher.init(Cipher.DECRYPT_MODE, aliceAesKey, aesParams);
+        
         byte[] recovered = aliceCipher.doFinal(ciphertext);
-        if (!java.util.Arrays.equals(cleartext, recovered))
+        
+        // проверка
+        if (!java.util.Arrays.equals("ПРИМЕР СООБЩЕНИЯ".getBytes(), recovered))
             throw new Exception("AES in CBC mode recovered text is " +
                     "different from cleartext");
         System.out.println("AES in CBC mode recovered text is " + 
-                "same as cleartext");
-    }
-    
-    private static void byte2hex(byte b, StringBuffer buf) {
-        char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
-                '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-        int high = ((b & 0xf0) >> 4);
-        int low = (b & 0x0f);
-        buf.append(hexChars[high]);
-        buf.append(hexChars[low]);
-    }
-
-    private static String toHexString(byte[] block) {
-        StringBuffer buf = new StringBuffer();
-        int len = block.length;
-        for (int i = 0; i < len; i++) {
-            byte2hex(block[i], buf);
-            if (i < len-1) {
-                buf.append(":");
-            }
-        }
-        return buf.toString();
+                "same as cleartext" + "\n" + 
+        		new String(recovered));
     }
